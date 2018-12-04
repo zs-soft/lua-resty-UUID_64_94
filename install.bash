b422499b31477fc3369836ee46dd049557283672
zs-soft <45423321+zs-soft@users.noreply.github.com>
2018-12-04 20:23:58 +0800

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,7 +1,6 @@
-cd clib
-make && cp libuuidx.so /usr/lib64/
+
+make && cp clib/libuuidx.so /usr/lib64/
 echo make ok
-cd ../lib
-cp resty/* /usr/local/share/lua/5.1/resty/
+cp lib/resty/* /usr/local/share/lua/5.1/resty/
 echo cp to /usr/local/share/lua/5.1/resty/
-cd ../../
+cd ../
